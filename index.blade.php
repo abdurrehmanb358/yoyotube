367dcd9c5019fec0a1ded050da5f2061e2e9b8fc
abdurrehmanb358 <115001451+abdurrehmanb358@users.noreply.github.com>
2023-05-23 16:59:36 +0500

    Add files via upload

---
+++ index.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Document</title>
+    <style>
+       body {
+  background-image: url(imges/pic2.jpg);
+}
+.hero {
+  background-image: url(path_to_your_image.jpg);
+  background-size: cover;
+  background-position: center;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  color: #ffffff;
+}
+
+.hero-content {
+  max-width: 600px;
+  margin: 0 auto;
+}
+
+.hero h1 {
+  font-size: 48px;
+  margin-bottom: 20px;
+}
+
+.hero p {
+  font-size: 24px;
+  margin-bottom: 40px;
+}
+
+.btn {
+  display: inline-block;
+  padding: 7px 10px;
+  background-color: #ffffff;
+  color: #333333;
+  text-decoration: none;
+  font-weight: bold;
+  border-radius: 4px;
+  transition: background-color 0.3s ease;
+  background-color: red;
+  border-radius:2px; 
+  color: white;
+}
+
+/* .btn:hover {
+  background-color: #eaeaea;
+} */
+
+
+    </style>
+</head>
+<body>
+    <div class="container">
+    <nav class="navbar">
+        <div class="container-fluid">
+         <h1> <a class="navbar-brand">YoYotube</a></h1>
+        <a href="Sginup" class="btn">Sginup</a>
+          
+                   
+        </div>
+      </nav>
+ </div>
+ <header class="hero">
+    <div class="hero-content">
+      <h1>Unlimited movies, TV shows, and more</h1>
+      <p>Watch anywhere. Cancel anytime.</p>
+      <p>Ready to watch? Enter your email to create or restart your membership.</p>
+ <input type="email">
+    </div>
+  </header>
+
+</div>
+</body>
+</html>
